c14c8e84902980faeab22c709b5ea507bdd506d8
DavidZele <david.zele@etrust.si>
2021-02-26 18:15:08 +0100

    Packages route with simple GET endpoints

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var packagesRouter = require('./routes/packages');
 
 var app = express();
 
@@ -37,6 +38,7 @@ app.use(function(req, res, next) {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/packages', packagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
