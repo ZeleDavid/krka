7f8e003c993093a5ea604790de41f2d1d520f3f6
CetinaLuka <luka.cetina@student.um.si>
2021-02-26 20:03:07 +0100

    feat: na strani skladiscnika se pokazejo narocila

--- frontend/src/views/Skladiscnik/Skladiscnik.js
+++ frontend/src/views/Skladiscnik/Skladiscnik.js
@@ -18,13 +18,27 @@ import {
     DialogTitle,
     List,
     ListItem,
-    ListItemText
+    ListItemText,
+    ListItemSecondaryAction,
+    Avatar,
+    Paper,
+    ListItemAvatar
 } from "@material-ui/core";
 import endpoints from '../../endpoints';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FolderIcon from '@material-ui/icons/Folder';
+import ReceiptIcon from '@material-ui/icons/Receipt';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles(theme => ({
     container: {
         backgroundColor: 'white',
+    },
+    potrdi: {
+        "&:hover": {
+            color: "green"
+        }
     }
 }));
 
@@ -43,7 +57,7 @@ function Skladiscnik() {
                 })
                 .then(response => {
                     console.log(response);
-                    if(response !== null){
+                    if (response !== null) {
                         setNarocila(response);
                         setIsNarocilaLoaded(true);
                     }
@@ -54,14 +68,49 @@ function Skladiscnik() {
         };
         naloziNarocila();
     }, []);
+    var loadingIndicator = (<div style={{textAlign: 'center', marginTop: '100px'}}><CircularProgress /></div>);
+    if(isNarocilaLoaded){
+        loadingIndicator = ('');
+    }
 
     return (
         <div>
             <Container>
-                <h2>Odobritev naročil</h2>
-                {narocila.map((narocilo) => 
-                    <p>{narocilo._id}</p>
-                )}
+                <Grid container>
+                    <Grid item xs={12} style={{ textAlign: "center" }}>
+                        <br />
+                        <Typography variant="h1" >
+                            Odobritev naročil
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    {loadingIndicator}
+                    <List>
+                        {narocila.map((narocilo) =>
+                            <Paper>
+                                <ListItem>
+
+
+                                    <ListItemAvatar>
+                                        <Avatar>
+                                            <ReceiptIcon />
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={"Narocilo " + narocilo.deliveryNumber}
+                                        secondary={narocilo.status+" • "+narocilo.submitionDate}
+                                    />
+                                    <ListItemSecondaryAction>
+                                        <IconButton edge="end" aria-label="delete">
+                                            <CheckIcon className={classes.potrdi} />
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                            </Paper>
+                        )}
+                    </List>
+                </Grid>
             </Container>
         </div>
     );
