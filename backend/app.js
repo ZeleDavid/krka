9ee99a5b20e7a82ad0bd1d60b0ec5f938f0c686f
DavidZele <david.zele@etrust.si>
2021-02-26 17:22:30 +0100

    changed port

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.set('port', process.env.PORT || 6969);
+app.listen(app.get('port'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
