bfec6d9665d2eedf8ca004be352fb520fc5e4fb2
CetinaLuka <luka.cetina@student.um.si>
2021-02-27 11:41:35 +0100

    feat: povezal dodajanje paketa z backendom

--- frontend/src/endpoints.js
+++ frontend/src/endpoints.js
@@ -1,6 +1,7 @@
 const endpoints = {
     obvestila: "http://localhost:6969",
     uporabniki: "http://localhost:6969",
-    narocila: "http://localhost:6969"
+    narocila: "http://localhost:6969",
+    paketi: "http://localhost:6969/packages/"
 }
 export default endpoints;
